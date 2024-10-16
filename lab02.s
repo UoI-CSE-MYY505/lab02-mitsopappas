61ffe85acd520dd9988b3cfb1e0b4e54164cbcdf
mitsopappas <jimmypappas7@gmail.com>
2024-10-16 21:24:29 +0300

    Update lab02.s

--- lab02.s
+++ lab02.s
@@ -10,8 +10,20 @@ array: .word 1, 0, 1, 12, 0, 1, 4
     li a2, 1
 prog:
 #-----------------------------
-# Write your code here!
-# Do not remove the prog label or write code above it!
+findLast_backwards_withPointers:
+    beq  a1, zero, ret0
+    slli s0, a1, 2  # offset of 1 word past the end of the array
+    add  s0, s0, a0 # full address of the above
+loop:
+    addi s0, s0, -4   # s0--
+    lw   t1, 0(s0)
+    beq  t1, a2, done
+    bne  s0, a0, loop
+ret0:
+    add  s0, zero, zero  # return address - not found
+done:
+    addi a7, zero, 10 
+    ecall
 #-----------------------------
 done:
     addi a7, zero, 10 
